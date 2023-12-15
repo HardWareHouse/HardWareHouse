71af754d18317f25dc9ac0def047ffc3938d84a5
iSaaMz <sami.assiakh@gmail.com>
2023-12-15 21:54:50 +0100

    Fix problème de route à l'interface administrateur

--- src/Controller/Admin/MainController.php
+++ src/Controller/Admin/MainController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-#[Route('/admin', name: 'app_admin')]
+#[Route('/admin')]
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_admin')]
