111907989e8d8cd8465b0c6b821f4cf37edf1341
iSaaMz <sami.assiakh@gmail.com>
2023-12-17 18:08:48 +0100

    Ajout script DataTable entreprise et paiement

---
+++ public/assets/js/paiementDatatable.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#paiementTable').DataTable({
+        "language": {
+            url: "/assets/json/French.json"
+        },
+        "lengthMenu": [10, 20, 30, 40, 50],
+        "paging": true,
+        "info": true,
+        "searching": true,
+        "order": [[0, 'asc']],
+    });
+});
