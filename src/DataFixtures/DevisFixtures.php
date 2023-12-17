739de81e1f5a084c77d092c14762a4fd277b7317
iSaaMz <sami.assiakh@gmail.com>
2023-12-17 17:11:24 +0100

    Ajout fixtures pour les devis, factures et produits

---
+++ src/DataFixtures/DevisFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Devis;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+class DevisFixtures extends Fixture implements OrderedFixtureInterface
+{
+    public function getOrder(): int
+    {
+        return 4;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 20; $i++) {
+            $devis = new Devis();
+            $devis->setNumero($faker->numerify('DEVIS####'))
+                  ->setDateCreation($faker->dateTimeThisYear())
+                  ->setStatus($faker->randomElement(['en attente', 'approuvé', 'refusé']))
+                  ->setTotal($faker->randomFloat(2, 100, 10000))
+                  ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeThisDecade()))
+                  ->setEntrepriseId($this->getReference('entreprise-'.rand(0, 9)));
+                //   ->setClientId($this->getReference('client-'.rand(0, 9)));
+
+
+            $manager->persist($devis);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/FactureFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Facture;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+class FactureFixtures extends Fixture implements OrderedFixtureInterface
+{
+    public function getOrder(): int
+    {
+        return 3;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+            $facture = new Facture();
+            $facture->setNumero($faker->numerify('FACT####'))
+                    ->setDateFacturation($faker->dateTimeThisYear())
+                    ->setDatePaiementDue($faker->dateTimeThisYear())
+                    ->setStatutPaiement($faker->randomElement(['payé', 'non payé', 'en retard']))
+                    ->setTotal($faker->randomFloat(2, 100, 10000))
+                    ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeThisDecade()))
+                    ->setEntrepriseId($this->getReference('entreprise-'.rand(0, 9)));
+                    // ->setClientId($this->getReference('client-'.rand(0, 9)));
+
+            $manager->persist($facture);
+        }
+
+        $manager->flush();
+    }
+}
