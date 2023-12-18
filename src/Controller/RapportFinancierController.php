86cb64ef50d3a9d264c28ed4a196fffb081a0690
iSaaMz <sami.assiakh@gmail.com>
2023-12-18 12:28:37 +0100

    Fix restriction admin et comptable pour les rapports

--- src/Controller/RapportFinancierController.php
+++ src/Controller/RapportFinancierController.php
@@ -5,14 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
-#[IsGranted('ROLE_COMPTABLE')]
 class RapportFinancierController extends AbstractController
 {
     #[Route('/rapport', name: 'app_rapport_financier')]
-    public function index(): Response
+    public function index(AuthorizationCheckerInterface $authChecker): Response    
     {
+        if (!$authChecker->isGranted('ROLE_ADMIN') && !$authChecker->isGranted('ROLE_COMPTABLE')) {
+            throw new AccessDeniedException('Access Denied.');
+        }
+        
         return $this->render('rapport_financier/index.html.twig', [
             'controller_name' => 'RapportFinancierController',
         ]);
