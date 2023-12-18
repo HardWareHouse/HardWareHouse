9423cb479ef468244f65c2be8b3825016cbf8b12
iSaaMz <sami.assiakh@gmail.com>
2023-12-18 22:09:55 +0100

    Modif form devis

--- src/Form/DevisType.php
+++ src/Form/DevisType.php
@@ -6,6 +6,9 @@ use App\Entity\Devis;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class DevisType extends AbstractType
 {
@@ -13,10 +16,22 @@ class DevisType extends AbstractType
     {
         $builder
             ->add('numero')
-            ->add('dateCreation')
-            ->add('status')
+            ->add('dateCreation', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'Approuvé' => 'Approuvé',
+                    'En attente' => 'En attente',
+                    'Refusé' => 'Refusé',
+                ],
+            ])
             ->add('total')
-            ->add('CreatedAt')
+            ->add('CreatedAt', DateTimeType::class, [
+                'widget' => 'single_text',
+                'data' => new \DateTime('now'),
+            
+            ])
             ->add('clientId')
             ->add('detailDevisId')
         ;
