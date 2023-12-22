0e0247b56c83cd96cbfc05a2565f38de3334aaf3
MoussaST <moussa21sr@gmail.com>
2023-12-22 17:23:45 +0100

    Suite Amélioration de la sécurité et Edition des messages d'erreurs

--- src/Controller/RapportFinancierController.php
+++ src/Controller/RapportFinancierController.php
@@ -7,10 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class RapportFinancierController extends AbstractController
 {
     #[Route('/{_locale<%app.supported_locales%>}/rapport', name: 'app_rapport_financier')]
+    // #[Security('is_granted("ROLE_ADMIN") or is_granted("ROLE_COMPTABLE")')]
     public function index(AuthorizationCheckerInterface $authChecker): Response    
     {
         if (!$authChecker->isGranted('ROLE_ADMIN') && !$authChecker->isGranted('ROLE_COMPTABLE')) {
