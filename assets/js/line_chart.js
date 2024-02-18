7c1ad880e7b19fb11998f339365aa328c9d431d2
Jay Bury <buryj15@gmail.com>
2024-02-18 19:40:29 +0100

    added charts for facture and devis, translations for charts, copy dashboard front to financial reports page, fix errors for chart years and add number formatting to chart axes

--- assets/js/line_chart.js
+++ assets/js/line_chart.js
@@ -1,214 +1,337 @@
 import * as echarts from "echarts";
 
-// METHODES DE PAIEMENT
-var methodes = echarts.init(document.getElementById("methodes"));
-var uniquePaymentMethods = [
-  ...new Set(paiementsData.map((paiement) => paiement.methodePaiement)),
-];
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize eCharts instance for line chart
+  var paymentsChart = echarts.init(document.getElementById("paiementsAnnee"));
 
-methodes.setOption({
-  title: {
-    text: "Methodes de paiement",
-    left: "center",
-  },
-  tooltip: {},
-  legend: { orient: "vertical", top: "center" },
-  series: [
-    {
-      name: "Nombre de paiements",
-      type: "pie", // Change the chart type to pie
-      radius: ["50%", "70%"], // Set inner and outer radius for the donut chart
-      avoidLabelOverlap: false,
-      label: {
-        show: false,
-        // position: "center",
-      },
-      //   emphasis: {
-      //     label: {
-      //       show: true,
-      //       fontSize: "30",
-      //       fontWeight: "medium",
-      //     },
-      //   },
-      labelLine: {
-        show: false,
-      },
-      data: uniquePaymentMethods.map((method) => ({
-        name: method,
-        value: paiementsData.filter((p) => p.methodePaiement === method).length,
-      })),
-    },
-  ],
-});
+  // Initialize eCharts instance for donut chart
+  var methodsChart = echarts.init(document.getElementById("methodes"));
 
-// PAIEMENTS PAR MOIS/ANNEE
-var revenue = echarts.init(document.getElementById("paiementsAnnee"));
+  var facturesChart = echarts.init(document.getElementById("facturesChart"));
 
-// Initialize monthlyData object with all months set to 0 for all years
-var monthlyData = {};
+  var devisChart = echarts.init(document.getElementById("devisChart"));
 
-// Extract the unique years from the data
-var uniqueYears = [
-  ...new Set(
-    paiementsData.map((paiement) =>
-      new Date(paiement.datePaiement).getFullYear()
-    )
-  ),
-];
-
-// Initialize monthly data for each year
-uniqueYears.forEach((year) => {
-  for (var i = 1; i <= 12; i++) {
-    var monthKey = year + "-" + (i < 10 ? "0" + i : i);
-    monthlyData[monthKey] = 0;
-  }
-});
-
-// Iterate through payments and update monthly data
-paiementsData.forEach(function (paiement) {
-  var date = new Date(paiement.datePaiement);
-  var year = date.getFullYear();
-  var month = date.getMonth() + 1; // Months are zero-based, so add 1
-  var key = year + "-" + (month < 10 ? "0" + month : month); // Ensure month format is 'MM'
-  monthlyData[key] += paiement.montant;
-});
-
-// Extract the keys (year-month) and corresponding montant totals
-var xAxisData = Object.keys(monthlyData);
-var montantData = Object.values(monthlyData);
-
-// Filter out future months from xAxisData and montantData
-var currentDate = new Date();
-var currentYear = currentDate.getFullYear();
-var currentMonth = currentDate.getMonth() + 1; // Add 1 because months are zero-based
-
-var filteredXAxisData = xAxisData.filter(function (yearMonth) {
-  var [year, month] = yearMonth.split("-");
-  return year < currentYear || (year == currentYear && month <= currentMonth);
-});
-
-var filteredMontantData = filteredXAxisData.map(function (yearMonth) {
-  return monthlyData[yearMonth];
-});
-
-revenue.setOption({
-  title: {
-    text: "Montant des paiements par mois",
-    left: "center", // Center the title
-    top: 20, // Adjust top margin
-  },
-  tooltip: {
-    trigger: "axis", // Show tooltip when hovering over data points
-    formatter: function (params) {
-      var value = params[0].value;
-      var yearMonth = params[0].axisValue;
-      return yearMonth + ": " + value.toFixed(2); // Format value as needed
-    },
-  },
-  xAxis: {
-    type: "category",
-    data: filteredXAxisData,
-  },
-  yAxis: {
-    type: "value",
-  },
-  series: [
-    {
-      type: "line",
-      data: filteredMontantData,
-    },
-  ],
-});
-
-// Extract years from the dataset
-var years = [];
-xAxisData.forEach(function (yearMonth) {
-  var year = yearMonth.split("-")[0];
-  if (!years.includes(year)) {
-    years.push(year);
-  }
-});
-// Sort the years array in ascending order
-years.sort();
-
-if (years.length > 1) {
-  var dropdownContainer = document.getElementById("dropdownContainer");
-  var dropdown = document.createElement("select");
-  dropdown.setAttribute("id", "yearDropdown");
-
-  years.forEach(function (year) {
-    var option = document.createElement("option");
-    option.setAttribute("value", year);
-    option.textContent = year;
-    dropdown.appendChild(option);
+  // Extract years from the paiements data
+  var years = paiementsData.map(function (paiement) {
+    return new Date(paiement.datePaiement).getFullYear();
   });
 
-  dropdown.addEventListener("change", function () {
-    var selectedYear = dropdown.value;
-    var filteredData = xAxisData.filter(function (yearMonth) {
-      return yearMonth.startsWith(selectedYear);
+  // Remove duplicate years
+  years = Array.from(new Set(years));
+
+  // Create dropdown options for years
+  var dropdownContainer = document.getElementById("dropdownContainer");
+  var dropdownHTML = '<select id="yearDropdown">';
+  // Sort the years numerically
+  years.sort(function (a, b) {
+    return b - a;
+  });
+  years.forEach(function (year) {
+    dropdownHTML += '<option value="' + year + '">' + year + "</option>";
+  });
+  dropdownHTML += "</select>";
+  dropdownContainer.innerHTML = dropdownHTML;
+
+  var selectedYear = new Date().getFullYear();
+  var downloadButton = document.getElementById("csvYear");
+  downloadButton.href = "/csv-methodes/" + selectedYear;
+
+  // Event listener for dropdown change
+  document
+    .getElementById("yearDropdown")
+    .addEventListener("change", function (event) {
+      selectedYear = parseInt(event.target.value);
+      updateCharts(selectedYear);
+      console.log(selectedYear);
+      downloadButton.href = "/csv-methodes/" + selectedYear;
     });
-    var filteredMontantData = filteredData.map(function (yearMonth) {
-      return monthlyData[yearMonth];
+
+  // Function to update charts based on selected year
+  function updateCharts(selectedYear) {
+    var paymentsPerMonth = new Array(12).fill(0); // Initialize array to hold payments per month
+
+    paiementsData.forEach(function (paiement) {
+      var paiementYear = new Date(paiement.datePaiement).getFullYear();
+      var paiementMonth = new Date(paiement.datePaiement).getMonth();
+
+      // Check if the paiement belongs to the selected year
+      if (paiementYear === selectedYear) {
+        paymentsPerMonth[paiementMonth] += paiement.montant;
+      }
     });
-    revenue.setOption({
+
+    // Update line chart data
+    var paymentsChartOption = {
+      tooltip: {
+        trigger: "axis",
+        formatter: function (params) {
+          var tooltipText = params[0].name + "<br/>"; // Assuming the x-axis represents time or categories
+
+          // Iterate over each data point in the tooltip
+          params.forEach(function (item) {
+            // Format the value as "00 000,00 €" and append it to the tooltip text
+            tooltipText +=
+              new Intl.NumberFormat("fr-FR").format(item.value) + " €<br/>";
+          });
+
+          return tooltipText;
+        },
+      },
       xAxis: {
-        data: filteredData,
+        type: "category",
+        data: [
+          translatedMonths.jan,
+          translatedMonths.feb,
+          translatedMonths.mar,
+          translatedMonths.apr,
+          translatedMonths.may,
+          translatedMonths.jun,
+          translatedMonths.jul,
+          translatedMonths.aug,
+          translatedMonths.sep,
+          translatedMonths.oct,
+          translatedMonths.nov,
+          translatedMonths.dec,
+        ],
+      },
+      yAxis: {
+        type: "value",
+        axisLabel: {
+          formatter: function (value) {
+            // Format the value as "00 000,00 €"
+            return new Intl.NumberFormat("fr-FR").format(value) + " €";
+          },
+        },
       },
       series: [
         {
-          data: filteredMontantData,
+          data: paymentsPerMonth,
+          type: "line",
         },
       ],
+    };
+    paymentsChart.setOption(paymentsChartOption);
+
+    // Prepare data for donut chart
+    var paymentMethods = {};
+    paiementsData.forEach(function (paiement) {
+      var paiementYear = new Date(paiement.datePaiement).getFullYear();
+      if (paiementYear === selectedYear) {
+        if (!paymentMethods[paiement.methodePaiement]) {
+          paymentMethods[paiement.methodePaiement] = 1;
+        } else {
+          paymentMethods[paiement.methodePaiement]++;
+        }
+      }
     });
-  });
 
-  // Append the dropdown to the container
-  dropdownContainer.appendChild(dropdown);
+    // Update donut chart data
+    var methodsChartOption = {
+      tooltip: {
+        trigger: "item",
+      },
+      legend: {
+        orient: "vertical",
+        right: 10,
+        data: Object.keys(paymentMethods),
+      },
+      series: [
+        {
+          type: "pie",
+          radius: ["50%", "70%"],
+          data: Object.keys(paymentMethods).map(function (method) {
+            return { value: paymentMethods[method], name: method };
+          }),
+        },
+      ],
+    };
+    methodsChart.setOption(methodsChartOption);
 
-  // Set the default value of the dropdown to the current year
-  var currentYear = new Date().getFullYear().toString();
-  dropdown.value = currentYear;
-}
+    var statusData = {
+      Payé: Array(12).fill(0),
+      "Non-payé": Array(12).fill(0),
+      "En retard": Array(12).fill(0),
+    };
 
-// Initially display only the data for the current year
-var initialFilteredData = filteredXAxisData.filter(function (yearMonth) {
-  return yearMonth.startsWith(currentYear);
-});
-var initialFilteredMontantData = initialFilteredData.map(function (yearMonth) {
-  return monthlyData[yearMonth];
-});
-revenue.setOption({
-  xAxis: {
-    data: initialFilteredData,
-  },
-  series: [
-    {
-      data: initialFilteredMontantData,
-    },
-  ],
-});
+    // Process Facture data to count status per month
+    facturesData.forEach(function (facture) {
+      var factureYear = new Date(facture.dateFacturation).getFullYear();
+      var factureMonth = new Date(facture.dateFacturation).getMonth();
 
-// Sort the xAxisData array based on year and month
-xAxisData.sort(function (a, b) {
-  // Split the year and month parts
-  var [yearA, monthA] = a.split("-");
-  var [yearB, monthB] = b.split("-");
+      // Check if the facture belongs to the selected year
+      if (factureYear === selectedYear) {
+        // Increment the count for the corresponding status and month
+        switch (facture.status) {
+          case "Payé":
+            statusData["Payé"][factureMonth]++;
+            break;
+          case "Non-payé":
+            statusData["Non-payé"][factureMonth]++;
+            break;
+          case "En retard":
+            statusData["En retard"][factureMonth]++;
+            break;
+        }
+      }
+    });
 
-  // Compare years
-  if (yearA !== yearB) {
-    return yearA - yearB;
-  } else {
-    // If years are equal, compare months
-    return monthA - monthB;
+    // Dynamically generate xAxis data array using translated month names
+    var xAxisData = [];
+    for (var i = 0; i < 12; i++) {
+      xAxisData.push(getTranslatedMonthName(i));
+    }
+
+    // Stacked bar chart options
+    var facturesChartOption = {
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+      legend: {
+        data: ["Payé", "Non-payé", "En retard"],
+      },
+      xAxis: {
+        type: "category",
+        data: xAxisData,
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          name: "Payé",
+          type: "bar",
+          stack: "status",
+          data: statusData["Payé"],
+        },
+        {
+          name: "Non-payé",
+          type: "bar",
+          stack: "status",
+          data: statusData["Non-payé"],
+        },
+        {
+          name: "En retard",
+          type: "bar",
+          stack: "status",
+          data: statusData["En retard"],
+        },
+      ],
+    };
+
+    facturesChart.setOption(facturesChartOption);
+
+    var devisStatusData = {
+      "En attente": Array(12).fill(0),
+      Approuvé: Array(12).fill(0),
+      Refusé: Array(12).fill(0),
+    };
+
+    // Process Facture data to count status per month
+    devisData.forEach(function (devis) {
+      var devisYear = new Date(devis.dateFacturation).getFullYear();
+      var devisMonth = new Date(devis.dateFacturation).getMonth();
+
+      // Check if the facture belongs to the selected year
+      if (devisYear === selectedYear) {
+        // Increment the count for the corresponding status and month
+        switch (devis.status) {
+          case "En attente":
+            devisStatusData["En attente"][devisMonth]++;
+            break;
+          case "Approuvé":
+            devisStatusData["Approuvé"][devisMonth]++;
+            break;
+          case "Refusé":
+            devisStatusData["Refusé"][devisMonth]++;
+            break;
+        }
+      }
+    });
+
+    // Dynamically generate xAxis data array using translated month names
+    var xAxisData = [];
+    for (var i = 0; i < 12; i++) {
+      xAxisData.push(getTranslatedMonthName(i));
+    }
+
+    // Stacked bar chart options
+    var devisChartOption = {
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+      legend: {
+        data: ["En attente", "Approuvé", "Refusé"],
+      },
+      xAxis: {
+        type: "category",
+        data: xAxisData,
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          name: "En attente",
+          type: "bar",
+          stack: "status",
+          data: devisStatusData["En attente"],
+        },
+        {
+          name: "Approuvé",
+          type: "bar",
+          stack: "status",
+          data: devisStatusData["Approuvé"],
+        },
+        {
+          name: "Refusé",
+          type: "bar",
+          stack: "status",
+          data: devisStatusData["Refusé"],
+        },
+      ],
+    };
+
+    devisChart.setOption(devisChartOption);
+
+    function getTranslatedMonthName(monthNumber) {
+      switch (monthNumber) {
+        case 0:
+          return translatedMonths.jan;
+        case 1:
+          return translatedMonths.feb;
+        case 2:
+          return translatedMonths.mar;
+        case 3:
+          return translatedMonths.apr;
+        case 4:
+          return translatedMonths.may;
+        case 5:
+          return translatedMonths.jun;
+        case 6:
+          return translatedMonths.jul;
+        case 7:
+          return translatedMonths.aug;
+        case 8:
+          return translatedMonths.sep;
+        case 9:
+          return translatedMonths.oct;
+        case 10:
+          return translatedMonths.nov;
+        case 11:
+          return translatedMonths.dec;
+        default:
+          return "";
+      }
+    }
   }
-});
-// Assuming you have a dropdown menu with id "yearDropdown"
-var dropdown = document.getElementById("yearDropdown");
 
-dropdown.addEventListener("change", function () {
-  var selectedYear = dropdown.value;
-  var downloadButton = document.getElementById("csvYear");
-  // Update the href attribute of the download button with the selected year
-  downloadButton.href = "/csv-methodes/" + selectedYear;
+  // Initial update of charts with the current year
+  var currentYear = new Date().getFullYear();
+  updateCharts(currentYear);
 });
--- src/Controller/RapportFinancierController.php
+++ src/Controller/RapportFinancierController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Repository\DevisRepository;
+use App\Repository\EntrepriseRepository;
+use App\Repository\FactureRepository;
 use App\Repository\PaiementRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
@@ -13,12 +17,21 @@ class RapportFinancierController extends AbstractController
 {
     #[Route('/{_locale<%app.supported_locales%>}/rapport', name: 'app_rapport_financier')]
     // #[Security('is_granted("ROLE_ADMIN") or is_granted("ROLE_COMPTABLE")')]
-    public function index(AuthorizationCheckerInterface $authChecker, PaiementRepository $repository): Response    
+    public function index(AuthorizationCheckerInterface $authChecker, PaiementRepository $paiementRepository, EntrepriseRepository $entrepriseRepository,TranslatorInterface $translator, FactureRepository $factureRepository, DevisRepository $devisRepository): Response    
     {
         if (!$authChecker->isGranted('ROLE_ADMIN') && !$authChecker->isGranted('ROLE_COMPTABLE')) {
             throw new AccessDeniedException('Access Denied.');
         }
-        $paiements = $repository->findAll();
+        $paiements = $paiementRepository->findAll();
+        $factures = $factureRepository->findAll();
+        $devis = $devisRepository->findAll();
+
+        $entreprises = $entrepriseRepository->createQueryBuilder('e')
+            ->select('e.id, e.nom')
+            ->distinct()
+            ->getQuery()
+            ->getResult();
+
         $paiementsArray = [];
         foreach ($paiements as $paiement) {
             $paiementsArray[] = [
@@ -26,19 +39,58 @@ class RapportFinancierController extends AbstractController
                 'datePaiement' => $paiement->getDatePaiement()->format('Y-m-d H:i:s'),
                 'montant' => $paiement->getMontant(),
                 'methodePaiement' => $paiement->getMethodePaiement(),
-                // Add other properties as needed
+                'entreprise' => [
+                    'id' => $paiement->getEntreprise()->getId(),
+                    'name' => $paiement->getEntreprise()->getNom(),
+                ],            
             ];
         }
+
+        $facturesData = [];
+
+        foreach ($factures as $facture) {
+            $facturesData[] = [
+                'id' => $facture->getId(),
+                'dateFacturation' => $facture->getDateFacturation()->format('Y-m-d H:i:s'),
+                'status' => $facture->getStatutPaiement(),           
+            ];
+        }
+
+         $devisData = [];
+
+        foreach ($devis as $devis) {
+            $devisData[] = [
+                'id' => $devis->getId(),
+                'dateFacturation' => $devis->getDateCreation()->format('Y-m-d H:i:s'),
+                'status' => $devis->getStatus(),           
+            ];
+        }
+
+        // Get translated month names
+        $translatedMonths = [
+            'jan' => $translator->trans('jan'),
+            'feb' => $translator->trans('feb'),
+            'mar' => $translator->trans('mar'),
+            'apr' => $translator->trans('apr'),
+            'may' => $translator->trans('may'),
+            'jun' => $translator->trans('jun'),
+            'jul' => $translator->trans('jul'),
+            'aug' => $translator->trans('aug'),
+            'sep' => $translator->trans('sep'),
+            'oct' => $translator->trans('oct'),
+            'nov' => $translator->trans('nov'),
+            'dec' => $translator->trans('dec')
+            // Repeat for other months
+        ];
         
         return $this->render('rapport_financier/index.html.twig', [
-            'controller_name' => 'RapportFinancierController', 'paiements' => json_encode($paiementsArray)
+            'controller_name' => 'RapportFinancierController', 'paiements' => json_encode($paiementsArray), 'translatedMonths' => json_encode($translatedMonths), 'entreprises' => json_encode($entreprises), 'facturesData' => json_encode($facturesData), 'devisData' => json_encode($devisData),
         ]);
     }
     #[Route(path: '/csv-methodes/{year}', name: 'app_csv_methodes')]
     public function csvMethodes(PaiementRepository $repository, $year): Response
 {
     $paiements = $repository->findPaymentsByYear($year);
-
     // Initialize CSV content with column names
     $csvContent = "Date Paiement; Montant; Methode Paiement\n";
 
