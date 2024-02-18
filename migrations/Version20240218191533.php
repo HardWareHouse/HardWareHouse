1bfb1b998257157c62c4057da3e0dc66ebde1dbe
iSaaMz <sami.assiakh@gmail.com>
2024-02-18 20:16:46 +0100

    Fix error migration

--- migrations/Version20240218191533.php
+++ migrations/Version20240218191533.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240217124515 extends AbstractMigration
+final class Version20240218191533 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,17 +20,17 @@ final class Version20240217124515 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE paiement DROP CONSTRAINT fk_b1dc7a1ea4aeafea');
-        $this->addSql('DROP INDEX idx_b1dc7a1ea4aeafea');
-        $this->addSql('ALTER TABLE paiement DROP entreprise_id');
+        $this->addSql('ALTER TABLE paiement ADD entreprise INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE paiement ADD CONSTRAINT FK_B1DC7A1ED19FA60 FOREIGN KEY (entreprise) REFERENCES entreprise (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_B1DC7A1ED19FA60 ON paiement (entreprise)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE paiement ADD entreprise_id INT NOT NULL');
-        $this->addSql('ALTER TABLE paiement ADD CONSTRAINT fk_b1dc7a1ea4aeafea FOREIGN KEY (entreprise_id) REFERENCES entreprise (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE INDEX idx_b1dc7a1ea4aeafea ON paiement (entreprise_id)');
+        $this->addSql('ALTER TABLE paiement DROP CONSTRAINT FK_B1DC7A1ED19FA60');
+        $this->addSql('DROP INDEX IDX_B1DC7A1ED19FA60');
+        $this->addSql('ALTER TABLE paiement DROP entreprise');
     }
 }
