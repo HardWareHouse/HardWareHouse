65dde8e575b71b9cacbae7bf3d8712bbb4484c6b
iSaaMz <sami.assiakh@gmail.com>
2024-02-19 20:34:17 +0100

    Ajout DataTables + filtres de recherches pour les entreprises côté admin

--- public/assets/js/entrepriseDatatable.js
+++ public/assets/js/entrepriseDatatable.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
-    $('#entrepriseTable').DataTable({
+    // Initialisation DataTable
+    var table = $('#entrepriseTable').DataTable({
         "language": {
-            url: "/assets/json/French.json"
+            "url": "/assets/json/French.json"
         },
         "lengthMenu": [10, 20, 30, 40, 50],
         "paging": true,
         "info": true,
         "searching": true,
-        "order": [[0, 'asc']],
+        "order": [[0, 'asc']]
+    });
+
+    // On applique ici le filtre de recherche pour chaque colonne de la table
+    $('#entrepriseTable thead tr:eq(1) th').each(function (i) {
+        $('input', this).on('keyup change', function () {
+            if (table.column(i).search() !== this.value) {
+                table
+                    .column(i)
+                    .search(this.value)
+                    .draw();
+            }
+        });
     });
 });
