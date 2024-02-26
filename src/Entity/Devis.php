8dcd3d05276b527914f09a2b29d3762065342f82
MoussaST <moussa21sr@gmail.com>
2024-02-26 22:58:07 +0100

    Fix suppression Devis

--- src/Entity/Devis.php
+++ src/Entity/Devis.php
@@ -39,7 +39,7 @@ class Devis
     #[ORM\JoinColumn(name: "client", referencedColumnName: "id")]
     private ?Client $clientId = null;
 
-    #[ORM\OneToMany(mappedBy: 'devis', targetEntity: DetailDevis::class , cascade: ['persist'])]
+    #[ORM\OneToMany(mappedBy: 'devis', targetEntity: DetailDevis::class , cascade: ['persist','remove'])]
     private Collection $detailDevis;
 
     public function __construct()
