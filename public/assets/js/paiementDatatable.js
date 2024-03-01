c88dc3fa5cce763dce9a282bafbd3e0c33e298b6
iSaaMz <sami.assiakh@gmail.com>
2024-03-01 12:34:54 +0100

    Empêche le tri des colonnes de DataTable lors de l'interaction avec les champs de recherche

--- public/assets/js/paiementDatatable.js
+++ public/assets/js/paiementDatatable.js
@@ -19,6 +19,10 @@ $(document).ready(function () {
 
   // On applique ici le filtre de recherche pour chaque colonne de la table
   $("#paiementTable thead tr:eq(1) th").each(function (i) {
+    $("input", this).on("click", function (e) {
+      e.stopPropagation();
+    });
+    
     $("input", this).on("keyup change", function () {
       if (table.column(i).search() !== this.value) {
         table.column(i).search(this.value).draw();
