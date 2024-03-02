26ecf3326f0bad8bfc22266493f3e408156efc2b
iSaaMz <sami.assiakh@gmail.com>
2024-03-02 20:25:52 +0100

    Ajout de contrainte d'unicité pour les catégories

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -7,8 +7,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
+#[UniqueEntity(fields: 'nom', message: 'Un nom de catégorie identique existe déjà.')]
 class Categorie
 {
     #[ORM\Id]
